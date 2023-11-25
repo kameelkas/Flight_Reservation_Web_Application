2dd68fcedeb8cfd34403b95677fdad6bf83104f7
kameelkas <kameelkas@gmail.com>
2023-11-24 18:42:36 -0700

    Started Domain Classes

---
+++ Customer.java
@@ -0,0 +1,14 @@
+class Customer{
+    private int customerID;
+    private String name;
+    private String emailAddr;
+    private String phoneNum;
+
+    public Customer(int cID, String name, String email, String phone){
+        this.customerID = cID;
+        this.name = name;
+        this. emailAddr = email;
+        this.phoneNum = phone;
+    }
+
+}

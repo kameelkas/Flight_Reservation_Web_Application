c75a8874634add0487188ee68775dffcba6db71a
kameelkas <kameelkas@gmail.com>
2023-11-25 08:24:13 -0700

    Added Data Member to Recognise Company Owned Aircrafts

--- Aircraft.java
+++ Aircraft.java
@@ -2,11 +2,13 @@ public class Aircraft {
     private int aircraftID;
     private String model;
     private int seatCapacity;
+    private boolean companyOwned;
 
-    public Aircraft(int aID, String model, int capacity){
+    public Aircraft(int aID, String model, int capacity, boolean owned){
         this.aircraftID = aID;
         this.model = model;
         this.seatCapacity = capacity;
+        this.companyOwned = owned;
     }
-    
+
 }

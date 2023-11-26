0320efe257c1b2a6210e719a61ec63ff0b93266b
mzafari2 <113462468+mzafari2@users.noreply.github.com>
2023-11-26 00:28:31 -0700

    login added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,17 +3,40 @@ import "./App.css";
 
 function App() {
   const [selectedOption, setSelectedOption] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggedInUser, setLoggedInUser] = useState(null); // New state for logged-in user
 
   const handleButtonClick = (option) => {
     setSelectedOption(option);
+    if (option === "Member") {
+      setShowModal(true);
+    }
   };
 
+  const handleModalClose = () => {
+    setShowModal(false);
+    setUsername("");
+    setPassword("");
+  };
+
+  const handleLogin = () => {
+    // Handle login logic with username and password
+    console.log("Username:", username);
+    console.log("Password:", password);
+    setLoggedInUser(username); // Set logged-in user
+    handleModalClose();
+  };
   return (
     <div className="container ">
       <div className="header">
-        <div id="title-holder">
-          <h2>Flight Booker</h2>
-        </div>
+        <h2 className="title">Flight Booker</h2>
+        {loggedInUser && (
+          <div className="user-info">
+            <span>Logged in as {loggedInUser}</span>
+          </div>
+        )}
       </div>
       <div className="main text-center mt-5">
         <h4>Continue as </h4>
@@ -38,7 +61,7 @@ function App() {
               type="button"
               onClick={() => handleButtonClick("Third Option")}
             >
-              Third Option
+              System Admin
             </button>
           </div>
         )}
@@ -62,12 +85,45 @@ function App() {
         )}
         {selectedOption === "Customer" && (
           <div className="d-grid gap-2 col-6 mx-auto">
-            <button className="btn btn-primary" type="button">
+            <button
+              className="btn btn-primary"
+              type="button"
+              onClick={() => handleButtonClick("Member")}
+            >
               Member
             </button>
             <button className="btn btn-primary" type="button">
               Guest
             </button>
+          </div>
+        )}
+        {showModal && selectedOption === "Member" && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={handleModalClose}>
+                &times;
+              </span>
+              <h2>Member Login</h2>
+              <form>
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  id="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="button" onClick={handleLogin}>
+                  Login
+                </button>
+              </form>
+            </div>
           </div>
         )}
       </div>

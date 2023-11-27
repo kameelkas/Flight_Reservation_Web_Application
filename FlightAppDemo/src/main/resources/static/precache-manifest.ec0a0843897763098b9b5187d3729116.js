264982ace85c5dffbbfa0d2c8a47a4564bdd4f88
Rohil1710 <rohil1710@gmail.com>
2023-11-27 16:46:31 -0700

    used npm run build to add react to static folder in springboot app - everything works

---
+++ FlightAppDemo/src/main/resources/static/precache-manifest.ec0a0843897763098b9b5187d3729116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f6b2b6938dbccb0bc1f241981409df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d561af133fafa2650ec",
+    "url": "/static/css/main.1fb848de.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "5d561af133fafa2650ec",
+    "url": "/static/js/main.93c087b6.chunk.js"
+  },
+  {
+    "revision": "cd2988c65fcca3ce3217",
+    "url": "/static/js/runtime~main.b45b9353.js"
+  }
+]);

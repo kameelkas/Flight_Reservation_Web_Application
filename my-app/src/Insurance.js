acdbc81b00b2fc6c3744f5fcbbd4f2b085d4578c
kameelkas <kameelkas@gmail.com>
2023-11-30 23:40:57 -0700

    IDK Insurance was Changed? Fixed Seat Hover Code I Think

--- my-app/src/Insurance.js
+++ my-app/src/Insurance.js
@@ -8,19 +8,26 @@ function Insurance({ onInsuranceSubmit }) {
   };
 
   return (
-    <div className="insurance-selection">
-      <div>
-        <h2>Insurance Selection</h2>
-        <br></br>
-        <div className="flex-container">
-          <div className="left-div">
-            <h4>Fully Covered</h4>
+    <div>
+      <div className="flex-container">
+        <div className="insurance-container">
+          <h2>Purchase an Insurance Plan</h2>
+          <br></br>
+          <div>
+            - Cancel or change flight for no additional fees
+            <br></br>- 1 free checked bag
+            <br></br>- Full refund upon cancellation
             <br></br>
-            <div>
-              
-Cancel or change flight for no additional fees<br></br>- 1 free checked bag<br></br>- Free seat selection<br></br>- Fully refundable<br></br><br></br><button>Select</button></div></div><div className="right-div"><h4>Partially Covered</h4><br></br><div>
-Cancel or change flight for a fee<br></br>- Checked bag for a fee<br></br>- Seat selection for a fee<br></br>- Partially refundable<br></br><br></br><button>Select</button></div></div></div><br></br></div><button onClick={handleInsuranceSubmit}>Skip Insurance and Pay</button></div>
-);
+            <br></br>
+            <button>Select</button>
+          </div>
+        </div>
+      </div>
+      <br></br>
+      <br></br>
+      <button onClick={handleInsuranceSubmit}>Skip Insurance and Pay</button>
+    </div>
+  );
 }
 
 export default Insurance;
